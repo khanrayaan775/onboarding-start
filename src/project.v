8e710d5bcf551d712a42c0b55fc10f47241768f7
Rayaan Khan <rayaankhan@Rayaans-MacBook-Pro.local>
2026-06-27 14:45:36 -0400

    fixed project.v again

--- src/project.v
+++ src/project.v
@@ -7,9 +7,9 @@
 
 module tt_um_uwasic_onboarding_Rayaan_Khan (
     input  wire [7:0] ui_in,    // Dedicated inputs
-    output reg [7:0] uo_out,   // Dedicated outputs
+    output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
-    output reg [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
@@ -18,8 +18,8 @@ module tt_um_uwasic_onboarding_Rayaan_Khan (
 
 
   // All output pins must be assigned. If not used, assign to 0.
-   // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+ // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+ // assign uio_out = 0;
   assign uio_oe  = 8'hFF;
 
   wire [7:0] en_reg_out_7_0;
