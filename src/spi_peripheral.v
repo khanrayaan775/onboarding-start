8a2be800caee676f71750bebaa238bcfcef9a111
Rayaan Khan <rayaankhan@Rayaans-MacBook-Pro.local>
2026-06-29 18:12:06 -0400

    single block, removed handshake, 15-bit shift_reg

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -29,23 +29,39 @@ module spi_peripheral (input clk, input rst_n, input sclk, input ncs, input copi
     wire sclk_rising = (sclk_2 ==1) & (sclk_3 ==0);
     wire ncs_posedge = (ncs_1 ==1) & (ncs_2 ==0);
 
-// next, need the block that starts transmission on ncs_2 == 0, does sampling at sclk_rising, does transmission logic
+// next, need the block that starts transmission on ncs_2 == 0, does sampling at sclk_rising, does transmission logic, AND transaction logc (edit)
     reg [14:0]shift_reg;
     reg [4:0]count;
-    reg transaction_finished;
-    reg transaction_ready;
+      // need a wire for "data", "address" , makes things cleaner
+    wire [7:0]data = shift_reg[7:0];
+    wire [6:0]address = shift_reg[14:8];
+
 
     always@(posedge clk or negedge rst_n)begin 
         // reset block 
         if(!rst_n) begin
             count <= 5'd0;
             shift_reg <= 15'd0;
-            transaction_ready <= 1'd0;
+            enregout7_0 <= 8'd0;
+            enregout15_8 <= 8'd0;
+            enpwmmode7_0 <= 8'd0;
+            enpwmmode15_8 <= 8'd0;
+            pwmdutycycle <= 8'd0;
+        
         end 
         // TRANSACTION LOGIC PLACED FIRST AFTER ANALYZING GDS WAVEFORM
-        else if(ncs_posedge && count == 5'd16) begin transaction_ready <= 1'd1; end 
-            // when the other block confirms the transation is complete, set transaction ready back to 0 (before) transmission starts again.
-        else if(transaction_finished) begin transaction_ready <= 1'd0; count <= 5'd0; end
+        else if(ncs_posedge && count == 5'd16) begin
+
+            case(address)
+            7'h00 : enregout7_0 <= data ;
+            7'h01 : enregout15_8 <= data;
+            7'h02 : enpwmmode7_0 <= data;
+            7'h03 : enpwmmode15_8 <= data;
+            7'h04 : pwmdutycycle <= data;
+            default: ;
+            endcase
+        count <= 5'd0;
+        end 
         // TRANSMISSION LOGIC PLACED SECOND AFTER ANALYZING GDS WAVEFORM
         // when ncs_2 = 0 (active low), check if rising edge on sclk, if yes, sample and increment till ncs_2 is 1 (transmision complete)
         else if(!ncs_2) begin
@@ -55,43 +71,9 @@ module spi_peripheral (input clk, input rst_n, input sclk, input ncs, input copi
                   end
                count <= count+1;
             end
-            // if transmission is complete, when ncs is on its positve edge (transitions), start the transaction.
         end 
         end
     
-    // second always block for transaction logic
-    // need a wire for "data", "address" , makes things cleaner
-    wire [7:0]data = shift_reg[7:0];
-    wire [6:0]address = shift_reg[14:8];
-    always @(posedge clk or negedge rst_n) begin
-
-        // reset block
-        if(!rst_n) begin transaction_finished <= 1'd0; 
-        enregout7_0 <= 8'd0;
-        enregout15_8 <= 8'd0;
-        enpwmmode7_0 <= 8'd0;
-        enpwmmode15_8 <= 8'd0;
-        pwmdutycycle <= 8'd0;
-        
-        end
-
-        // if transaction start approval from first block 
-        else if(transaction_ready) begin 
-            // need to verify "count", verify valid address, move to correct output register using case 
-            case(address)
-            7'h00 : enregout7_0 <= data ;
-            7'h01 : enregout15_8 <= data;
-            7'h02 : enpwmmode7_0 <= data;
-            7'h03 : enpwmmode15_8 <= data;
-            7'h04 : pwmdutycycle <= data;
-            default: ;
-            endcase
-        transaction_finished <= 1;
-        // "handshake mechanism" -> first block sees the line above, sets tready to 0, second block then executes below, sets tfinished to 0
-        end else if(transaction_finished && !transaction_ready) begin 
-            transaction_finished <= 0;
-        end 
-        
-
-    end
+    // second always with transaction logic DELETED, moved into FIRST
+  
  endmodule
